7fa5178695acad0d08b42c389388b705773bd5bc
Ismail Bourbie <bourbieismail@gmail.com>
2019-04-11 14:47:37 +0200

    start with first expressions

---
+++ racket files/courses/expressions pt1.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |expressions pt1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(+ 3 4) ; 3 + 4 = 7
+(/ 12 (* 2 3)) ; 12 / (2*3) = 2
+(sqr 3) ; 3² = 9
+(sqrt 16); sqrt(16) = 4
