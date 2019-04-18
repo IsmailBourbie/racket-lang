e608464c311014c482301ffd2e71187d82dbe6b3
Ismail Bourbie <bourbieismail@gmail.com>
2019-04-18 15:40:13 +0200

    use if statment

--- racket files/courses/booleans.rkt
+++ racket files/courses/booleans.rkt
@@ -1,11 +1,20 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname booleans) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+;(define WIDTH  100)
+;(define HEIGHT 100)
 
-(define WIDTH  100)
-(define HEIGHT 100)
+;(> WIDTH HEIGHT) ; return a bool value: false
+;(>= WIDTH HEIGHT) ; return a bool value: true
 
-(> WIDTH HEIGHT) ; return a bool value: false
-(>= WIDTH HEIGHT) ; return a bool value: true
+;(string=? "foo" "bar"); compare two string
 
-(string=? "foo" "bar"); compare two string
+
+(define I1 (rectangle 10 20 "solid" "blue"))
+(define I2 (rectangle 20 10 "solid" "red"))
+
+(if (< (image-width I1)
+       (image-height I1))
+    "tall"; if width < height print "tall"
+    "wide"); else print "wide"
